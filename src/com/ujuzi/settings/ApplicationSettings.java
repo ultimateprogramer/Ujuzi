d59ded0c11c593fbcbb52af0492d6a4e2c246e8b
clarkritchie <clarkritchie@yahoo.com>
2010-09-14 15:23:37 -0400

    Added GNU GPL disclaimers, updated headers, added READMEs and other legal documentation.

--- src/com/ujuzi/settings/ApplicationSettings.java
+++ src/com/ujuzi/settings/ApplicationSettings.java
@@ -1,6 +1,24 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2010 Ahmed Mohammed Maaway
+ *
+ * This file is part of Ujuzi.
+ *
+ * Ujuzi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ujuzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ujuzi.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional information can be found in README.txt or at
+ * <http://www.ujuziapp.com/>.
+ *
  */
 
 package com.ujuzi.settings;
